e7e887a6c909f51a68ec79b273fb0ff825ddb431
Pablo Rausch <pablorausch@gmail.com>
2020-01-04 17:18:07 -0300

    single pattern

---
+++ RegularExpressionMatching.Logic/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace RegularExpressionMatching.Logic
+{
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public bool IsMatch(string s, string p)
+        {
+            var pattern = new Stack<char>(p);
+            var input = new Stack<char>(s);
+
+            while (pattern.Count > 0)
+            {
+                var expected = pattern.Pop();
+
+                if (input.Count == 0)
+                    return false;
+
+                var actual = input.Pop();
+
+                if (expected != actual)
+                    return false;
+            }
+
+            if (input.Count > 0)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ RegularExpressionMatching.Tests/RegularExpressionMatchingTests.cs
@@ -0,0 +1,18 @@
+﻿namespace RegularExpressionMatching.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RegularExpressionMatching.Logic;
+
+    [TestClass]
+    public class RegularExpressionMatchingTests
+    {
+        [DataTestMethod]
+        [DataRow("aa","a", false)]
+        public void Examples(string s, string p, bool expected)
+        {
+            var sut = new Solution();
+            var result = sut.IsMatch(s, p);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
