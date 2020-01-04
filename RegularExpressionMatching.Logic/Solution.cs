4cf4d60eeb9f6145db72b0ac7ee6267ccb445d5e
Pablo Rausch <pablorausch@gmail.com>
2020-01-04 18:31:19 -0300

    added some cases

--- RegularExpressionMatching.Logic/Solution.cs
+++ RegularExpressionMatching.Logic/Solution.cs
@@ -6,41 +6,70 @@
     {
         public bool IsMatch(string s, string p)
         {
-            var pattern = new Stack<char>(p);
-            var input = new Stack<char>(s);
+            var pattern = new Queue<char>(p);
+            var input = new Queue<char>(s);
+
+            var lastMultipleDequeued = '0';
 
             while (pattern.Count > 0)
             {
-                var expected = pattern.Pop();
+                var expected = pattern.Dequeue();
+                var next = '0';
 
-                if (expected == '*')
+                if (pattern.Count > 0)
                 {
-                    expected = pattern.Pop();
+                    next = pattern.Peek();
 
-                    var repeated = expected;
-
-                    while (repeated == expected && input.Count > 0)
-                        repeated = input.Pop();
-
-                    if (input.Count == 0)
-                        continue;
-                    else if (repeated != '0')
-                        input.Push(repeated);
+                    if (next == '*')
+                        pattern.Dequeue();
                 }
 
-                if (input.Count == 0)
-                    return false;
+                if (next == '*')
+                {
+                    if (expected == '.')
+                    {
+                        if (pattern.Count == 0)
+                            return true;
 
-                var actual = input.Pop();
+                        var until = pattern.Peek();
 
-                if (expected != actual)
-                    return false;
+                        if (pattern.Count > 1 && pattern.ToArray()[1] == '*')
+                        {
+                            pattern.Dequeue();
+                            until = pattern.Dequeue();
+                        }
+
+                        while (input.Count > 0 && !this.Valid(input.Peek(), until))
+                            lastMultipleDequeued = input.Dequeue();
+                    }
+                    else
+                    {
+                        while (input.Count > 0 && this.Valid(input.Peek(), expected))
+                            lastMultipleDequeued = input.Dequeue();
+                    }
+                }
+                else
+                {
+                    if (input.Count == 0)
+                        return expected == lastMultipleDequeued;
+
+                    if (!this.Valid(input.Dequeue(), expected))
+                        return false;
+                }
             }
 
             if (input.Count > 0)
-                return false;
+                return this.Valid(input.Dequeue(), lastMultipleDequeued);
 
             return true;
+        }
+
+        private bool Valid(char actual, char expected)
+        {
+            if (expected == '.')
+                return true;
+
+            return expected == actual;
         }
     }
 }
--- RegularExpressionMatching.Tests/RegularExpressionMatchingTests.cs
+++ RegularExpressionMatching.Tests/RegularExpressionMatchingTests.cs
@@ -7,8 +7,18 @@
     public class RegularExpressionMatchingTests
     {
         [DataTestMethod]
-        [DataRow("aa","a", false)]
-        [DataRow("aa", "a*", true)]
+        //[DataRow("aa", "a", false)]
+        //[DataRow("aa", "a*", true)]
+        //[DataRow("ab", ".*", true)]
+        //[DataRow("aab", "c*a*b", true)]
+        //[DataRow("mississippi", "mis*is*p*.", false)]
+        //[DataRow("mississippi", "mis*is*ip*.", true)]
+        //[DataRow("aaa", "ab*ac*a", true)]
+        //[DataRow("aaa", "ab*a*c*a", true)]
+        //[DataRow("ab", ".*c", false)]
+        //[DataRow("bbbba", ".*a*a", true)]
+        //[DataRow("ab", ".*..", true)]
+        [DataRow("abbbcd", "ab*bbbcd", true)]
         public void Examples(string s, string p, bool expected)
         {
             var sut = new Solution();
